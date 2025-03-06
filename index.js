3d406f975c1ce1d9ff3b023791c3dba8f383e614
Asif <asif2000nm@gmail.com>
2025-03-06 23:30:02 +0530

    step 1

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const add = (nums) => {
-  return 0;
+  if (nums.length === 0) {
+    return 0;
+  }
+  if (nums.length === 1) {
+    return parseInt(nums);
+  }
+  if (nums.length === 3) {
+    return parseInt(nums[0]) + parseInt(nums[nums.length - 1]);
+  }
 };
 
 module.exports = { add };
--- index.test.js
+++ index.test.js
@@ -3,5 +3,12 @@ const assert = require("assert/strict");
 const { add } = require("./index");
 
 test("", () => {
-  return assert.equal(add([]), 0);
+  return assert.equal(add(""), 0);
+});
+
+test("", () => {
+  return assert.equal(add("1"), 1);
+});
+test("", () => {
+  return assert.equal(add("1,5"), 6);
 });
